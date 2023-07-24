b8332046741db5473761df4e91114df34bacc8ee
serranxxx <albserrano8@gmail.com>
2023-07-24 11:03:39 -0600

    dependencies

---
+++ src/AvatarStudio.js
@@ -0,0 +1,20 @@
+
+import { Image, Image2 } from './layout/Image';
+
+function AvatarStudio() {
+  return (
+    <div className="App"
+      style={{
+        display: 'flex', alignItems: 'center', justifyContent: 'center'
+      }}>
+
+      <Image />
+      <Image2 />
+
+
+
+    </div>
+  );
+}
+
+export default AvatarStudio;
